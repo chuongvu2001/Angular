c46206e30830194a857f5b684eb7688f171d5b59
chuongvu2001 <chuongvu2806@gmail.com>
2021-03-18 22:27:00 +0700

    update

--- src/app/screens/hero-list/hero-list.component.spec.ts
+++ src/app/screens/hero-list/hero-list.component.spec.ts
@@ -18,7 +18,6 @@ describe('HeroListComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
